5d5ce0b94bb71c7ab6784330522b52f58af45802
kevin ahmad <kevinahmad.f@gmail.com>
2020-11-03 20:20:45 +0700

    add about us and konten component

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "",
+  headline: "About Us",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  buttonLabel: "Learn More",
+  imgStart: false,
+  img: require("../../images/logo.png"),
+  alt: "logo",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,7 +1,68 @@
 import React from "react";
+import { Button } from "../ButtonElement";
+import {
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Column1,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  Column2,
+  Img,
+  ImgWrap,
+  BtnWrap,
+} from "./InfoElements";
 
-const InfoSection = () => {
-  return <div></div>;
+const InfoSection = ({
+  lightText,
+  lightBg,
+  id,
+  imgStart,
+  headline,
+  topLine,
+  darktext,
+  description,
+  buttonLabel,
+  img,
+  alt,
+}) => {
+  return (
+    <>
+      <InfoContainer lightBg={lightBg} id={id}>
+        <InfoWrapper>
+          <InfoRow imgStart={imgStart}>
+            <Column1>
+              <TextWrapper>
+                <TopLine>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headline}</Heading>
+                <Subtitle darktext={darktext}>{description}</Subtitle>
+                <BtnWrap>
+                  <Button
+                    primary="true"
+                    to="home"
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact="true"
+                    offset={-80}
+                  >
+                    {buttonLabel}
+                  </Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={img} alt={alt} />
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  );
 };
 
 export default InfoSection;
