f77cde7d70b254ffeab5cdf9d64c53496ffc17af
Jonathan <jonathan.sendiko@gmail.com>
2020-11-17 16:08:52 +0700

    deployment

--- index.js
+++ index.js
@@ -33,5 +33,5 @@ mongoose.connect(
   }
 );
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
